a033f1902089273feba90bfe5d932b17cb9fd6f6
websmartcode <desenvolvimentosmartcode@gmail.com>
2024-02-15 11:53:58 -0300

    deploy

--- src/config.js
+++ src/config.js
@@ -1,33 +1,32 @@
-// Load environment variables from .env file
+// config.js
 require('dotenv').config()
 
-// setup global const
-const sessionFolderPath = process.env.SESSIONS_PATH || './sessions'
-const enableLocalCallbackExample = (process.env.ENABLE_LOCAL_CALLBACK_EXAMPLE || '').toLowerCase() === 'true'
-const globalApiKey = 'smartcode.com.br';
-const baseWebhookURL = process.env.BASE_WEBHOOK_URL
+const sessionFolderPath = process.env.SESSIONS_PATH
+const enableLocalCallbackExample = process.env.ENABLE_LOCAL_CALLBACK_EXAMPLE === 'true'
+const globalApiKey = process.env.GLOBAL_API_KEY || 'smartcode.com.br';
+const baseWebhookURL = process.env.BASE_WEBHOOK_URL || 'http://example.com/webhook'
 const maxAttachmentSize = parseInt(process.env.MAX_ATTACHMENT_SIZE) || 10000000
-const setMessagesAsSeen = (process.env.SET_MESSAGES_AS_SEEN || '').toLowerCase() === 'true'
+const setMessagesAsSeen = process.env.SET_MESSAGES_AS_SEEN === 'true'
 const disabledCallbacks = process.env.DISABLED_CALLBACKS ? process.env.DISABLED_CALLBACKS.split('|') : []
-const enableSwaggerEndpoint = (process.env.ENABLE_SWAGGER_ENDPOINT || '').toLowerCase() === 'true'
+const enableSwaggerEndpoint = process.env.ENABLE_SWAGGER_ENDPOINT === 'true'
 const webVersion = process.env.WEB_VERSION
 const webVersionCacheType = process.env.WEB_VERSION_CACHE_TYPE || 'none'
 const rateLimitMax = process.env.RATE_LIMIT_MAX || 1000
 const rateLimitWindowMs = process.env.RATE_LIMIT_WINDOW_MS || 1000
-const recoverSessions = (process.env.RECOVER_SESSIONS || '').toLowerCase() === 'true'
+const recoverSessions = process.env.RECOVER_SESSIONS === 'true'
 
 module.exports = {
-  sessionFolderPath,
-  enableLocalCallbackExample,
-  globalApiKey,
-  baseWebhookURL,
-  maxAttachmentSize,
-  setMessagesAsSeen,
-  disabledCallbacks,
-  enableSwaggerEndpoint,
-  webVersion,
-  webVersionCacheType,
-  rateLimitMax,
-  rateLimitWindowMs,
-  recoverSessions
+    sessionFolderPath,
+    enableLocalCallbackExample,
+    globalApiKey,
+    baseWebhookURL,
+    maxAttachmentSize,
+    setMessagesAsSeen,
+    disabledCallbacks,
+    enableSwaggerEndpoint,
+    webVersion,
+    webVersionCacheType,
+    rateLimitMax,
+    rateLimitWindowMs,
+    recoverSessions
 }
