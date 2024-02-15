cc61ad5c5d80e62f4c77c8162c5b1e90227e7889
websmartcode <desenvolvimentosmartcode@gmail.com>
2024-02-15 11:48:44 -0300

    deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-require('dotenv').config(); // Make sure this line is at the top
+require('dotenv').config(); // Certifique-se de que esta linha está no topo
 
+// Verifique se BASE_WEBHOOK_URL está definida
 if (!process.env.BASE_WEBHOOK_URL) {
     console.error('BASE_WEBHOOK_URL environment variable is not available. Exiting...');
     process.exit(1);
@@ -8,14 +9,14 @@ if (!process.env.BASE_WEBHOOK_URL) {
 const app = require('./src/app');
 const { baseWebhookURL } = require('./src/config');
 
-// Start the server
-const port = process.env.PORT || 3000;
-
-// Check if BASE_WEBHOOK_URL environment variable is available
+// Verifique se a variável baseWebhookURL está definida
 if (!baseWebhookURL) {
-    console.error('BASE_WEBHOOK_URL environment variable is not available. Exiting...');
-    process.exit(1); // Terminate the application with an error code
+    console.error('BASE_WEBHOOK_URL from config is not available. Exiting...');
+    process.exit(1); // Encerre o aplicativo com um código de erro
 }
+
+// Inicie o servidor
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
